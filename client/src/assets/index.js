252a10549e74ea111e6c388bdd0822cc204e63e5
dtk1 <2018daulet@mail.ru>
2022-08-08 20:06:46 +0600

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,5 @@
+export const APP_LOGO_URL = {
+  link: process.env.PUBLIC_URL + "/img/1234.png",
+  width: "200px",
+  height: "200px",
+};
---
+++ client/src/config/index.js
@@ -0,0 +1,2 @@
+export const GOOGLE_CLIENT_ID =
+  "913850901008-1i4imbvgb96qsjgi4o2cshconq2rmql9.apps.googleusercontent.com";
